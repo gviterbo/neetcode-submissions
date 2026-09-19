ab00483d5725e1f3541caef2f219af1c5791c4f0
Gabriel Viterbo <144609874+gviterbo@users.noreply.github.com>
2026-09-19 04:50:48 +0200

    Add: decode-ways - submission-1

---
+++ Data Structures & Algorithms/decode-ways/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        n = len(s)
+        nums = set()
+        for x in range(1,27):
+            nums.add(str(x))
+        
+        ans = [0]*n
+        if s[0] in nums:
+            ans[0] = 1
+
+        for i in range(1,n):
+            x = 1
+            if s[i-1: i+1] in nums:
+                if i > 2: x = ans[i-2]
+                ans[i] += x
+            if s[i] in nums:
+                ans[i] += ans[i-1]
+        return ans[n-1]
+            
